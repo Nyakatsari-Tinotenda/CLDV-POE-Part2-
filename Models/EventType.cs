a0e354ea5931500bec6c1e475a2fb7bc742cdee6
Tinotenda <tinonyakatsari@gmail.com>
2025-06-23 17:40:17 +0200

    POE part 3

---
+++ Models/EventType.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace poe.Models
+{
+    public class EventTypes
+    {
+        public int EventTypeId { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; } // e.g., "Conference", "Wedding"
+
+        public ICollection<Event> Events { get; set; }
+    }
+}
---
+++ Models/Venue.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace poe.Models
+{
+    public class Venue
+    {
+        public int VenueId { get; set; }
+
+        [Required]
+        public string VenueName { get; set; }
+
+        [Required]
+        public string Location { get; set; }
+
+        [Required]
+        [Range(1, int.MaxValue)]
+        public int Capacity { get; set; }
+
+        public string? ImageUrl { get; set; }
+
+        // New property to track availability status of the venue
+        public bool IsAvailable { get; set; } = true;
+
+        public ICollection<Booking>? Bookings { get; set; }
+    }
+}
